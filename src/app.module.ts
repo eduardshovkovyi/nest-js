fcb4c8649b45f4e51f1212f9ac0e9cfee688159b
eduardshovkovyi <edik.shovkovyi@gmail.com>
2023-08-02 18:22:04 +0200

    create todo module and entity and add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './modules/todo/todo.module';
+import databaseConfig from '../database.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
